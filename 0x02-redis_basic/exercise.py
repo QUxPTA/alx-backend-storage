cbe79c8687b2126508b879f4ac318d7d21361691
kibuchijw <kibuchijw@gmail.com>
2024-05-15 21:18:36 +0300

    Update the cache class to include get methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,8 +4,8 @@ Redis module
 """
 
 import uuid
+from typing import Union, Callable, Any
 import redis
-from typing import Union
 
 
 class Cache:
@@ -35,3 +35,46 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Any:
+        """
+        Retrieve data from the cache using the provided key.
+
+        Args:
+            key: The key used to retrieve the data from the cache.
+            fn: An optional callable function to convert the retrieved data.
+                If provided, the function will be applied to retrieved data.
+
+        Returns:
+            The retrieved data from the cache.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Union[str, bytes]:
+        """
+        Retrieve a string value from the cache using the provided key.
+
+        Args:
+            key: The key used to retrieve the data from the cache.
+
+        Returns:
+            The retrieved string value from the cache.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, bytes]:
+        """
+        Retrieve an integer value from the cache using the provided key.
+
+        Args:
+            key: The key used to retrieve the data from the cache.
+
+        Returns:
+            The retrieved integer value from the cache.
+        """
+        return self.get(key, fn=int)
